4306094942bd536e55a4d08d49ea7ea644cebe47
Zack Kirchin <zacharykirchin@gmail.com>
2022-09-21 08:40:40 -0400

    Updates

---
+++ precache-manifest.027ae41c500f3bca0de7d7ec6813f835.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9724f498fa0935b60d0f",
+    "url": "/zc_bronco/static/css/main.90a91033.chunk.css"
+  },
+  {
+    "revision": "9724f498fa0935b60d0f",
+    "url": "/zc_bronco/static/js/main.c0fd6114.chunk.js"
+  },
+  {
+    "revision": "dd31cfda206252e33587",
+    "url": "/zc_bronco/static/js/runtime~main.7eb2a603.js"
+  },
+  {
+    "revision": "316cc844a04bf0ef5f9d",
+    "url": "/zc_bronco/static/js/2.926424c2.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/zc_bronco/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/zc_bronco/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "34c0d11c4856a32344b6a00bccd7463c",
+    "url": "/zc_bronco/index.html"
+  }
+];
